d007e27c992f20c3277277cd3fed3da3142f28bc
alejandrosma <109106837+alejandrosma@users.noreply.github.com>
2022-07-16 20:30:26 +0200

    Add files via upload

---
+++ USPEX/Ca4Al2Si2O20H16/INPUTS/Specific/siesta2gulp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Coger los vectores de celda
+a=$(sed '5q;d' STRUC | tr -s " " | cut -d " " -f 2)
+b=$(sed '6q;d' STRUC | tr -s " " | cut -d " " -f 3)
+c=$(sed '7q;d' STRUC | tr -s " " | cut -d " " -f 4)
+
+#Copiar opciones de gulp al input de gulp
+cat goptions > input_gulp.in
+echo "cell" >> input_gulp.in
+echo $a $b $c "90.0 90.0 90.0" >> input_gulp.in
+echo "fractional" >> input_gulp.in
+awk 'NR==11, NR==54' STRUC | awk '{ print $4" "$1" "$2" "$3 }' | sed 's/^1/Ca/' | sed 's/^2/Al/' | sed 's/^3/Si /' | sed 's/^4/O /' | sed 's/^5/H /'| awk '{ print $1" core "$2" "$3" "$4 }' >> input_gulp.in
+echo " " >> input_gulp.in
+
+cat ginput >> input_gulp.in
+
